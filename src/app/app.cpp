323e2753a3f1dbc0edaf1dc7cb52ef1704592d2c
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2024-03-09 16:03:27 -0400

    feat: add move camera and apply zoom

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -7,20 +7,53 @@ void App::Run() {
 
     SetTargetFPS(kFPS);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         // Update
         // =================================
+        MoveCamera();
+        ApplyZoom();
         board_.Update();
 
         // Draw
         // ==================================
         BeginDrawing();
+        BeginMode2D(camera_);
+
         board_.Draw();
+
+        EndMode2D();
         EndDrawing();
     }
 
     Close();
+}
+
+void App::ApplyZoom() {
+    auto scroll = GetMouseWheelMove();
+    if (scroll == 0) return;
+
+    /*
+     * The following was extracted direct from raylib examples
+     */
+
+    auto mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera_);
+    camera_.offset = GetMousePosition();
+    camera_.target = mouseWorldPos;
+    const float zoomIncrement = 0.125f;
+    camera_.zoom += scroll * zoomIncrement;
+
+    if (camera_.zoom < zoomIncrement) camera_.zoom = zoomIncrement;
+}
+
+void App::MoveCamera() {
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+    {
+        auto delta = GetMouseDelta();
+        delta = Vector2Scale(delta, -1.0f/camera_.zoom);
+        if (delta.x == 0 && delta.y == 0) return;
+
+        camera_.target = Vector2Add(camera_.target, delta);
+    }
 }
 
 void App::Close() const {
--- src/app/app.h
+++ src/app/app.h
@@ -4,14 +4,18 @@
 #include <cstdint>
 #include <string>
 #include "../render/board.h"
+#include <raymath.h>
 
 class App {
 public:
     App(std::string title, uint32_t width = 720, uint32_t height = 480)
-        : title_(title), width_(width), height_(height) {}
+        : title_(title), width_(width), height_(height),
+          camera_(Camera2D{.zoom = 1.0f}) {}
     void Run();
 
 private:
+    void ApplyZoom();
+    void MoveCamera();
     void Close() const;
 
 private:
@@ -19,6 +23,7 @@ private:
     uint32_t width_;
     uint32_t height_;
 
+    Camera2D camera_;
     Board board_;
 
 private:
